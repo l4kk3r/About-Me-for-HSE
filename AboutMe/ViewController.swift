6b5b43714385441c814c3140dba53befa7b71e41
l4kk3r <66743739+l4kk3r@users.noreply.github.com>
2020-07-07 18:37:50 +0300

    Add files via upload

---
+++ AboutMe/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AboutMe
+//
+//  Created by Admin on 07.07.2020.
+//  Copyright © 2020 chram studio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
